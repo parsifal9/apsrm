9166fa2112ae19905f77e91dced963f55018d663
Simon Knapp <simon.knapp@data61.csiro.au>
2023-03-21 11:51:44 +1100

    Twerks for choral event.

--- workplaces/Chorus_USA/utils.py
+++ workplaces/Chorus_USA/utils.py
@@ -16,118 +16,79 @@ from enum import Enum
 from math import floor
 from typing import Callable
 from scipy.stats import poisson
-from apsrm import (
-    Person,
-    Workplace,
-    Box)
-from apsrm.ext.office import (
-#    MeetingGenerator,
-#    GatheringVisits,
-#    LunchGenerator,
-#    ToiletBreakGenerator,
-#    EgressGenerator,
-    WorkingGenerator)
+from apsrm import Person, Gathering, Workplace, Box, GatheringGenerator
+from apsrm.ext.office import MeetingGenerator
 from apsrm.ext.simulation import EmissionsCalculator
 
 
-#AVERAGE_VISITORS_PER_MEETING = .25
-
-
 class WORKER_TYPE(Enum):
-    CHORUS       = 1
+    CHORUS = 1
 
 class BOX_TYPE(Enum):
-    OPEN_PLAN      = 1
-    HVAC      = 8
+    HALL = 1
+    HVAC = 8
 
 class GATHERING_TYPE(Enum):
     MEETING = 1
 
 
-class Visitor(Person):
+class Singer(Person):
     def __init__(self, *args, **kwargs):
-        kwargs['role'] = WORKER_TYPE.VISITOR
         super().__init__(*args, **kwargs)
 
     def _generate_work_day(self, period):
         return None
 
+class ChoralPractice(GatheringGenerator):
+    def __init__(self, box):
+        self.box = box
 
-#_VISITOR_COUNT_DIST = poisson(AVERAGE_VISITORS_PER_MEETING)
+    def create_gatherings(self, workplace, period, current_gatherings):
+        persons = workplace.persons
+        gathering = Gathering(start=9., end=13., box=self.box)
+        for person in workplace.persons:
+            gathering.add_participant(person)
 
-#def visitor_count_generator(gatherings):
-#    return _VISITOR_COUNT_DIST.rvs(size=len(gatherings))
+        return [gathering]
 
 
-# ### Set the Model up
-#
-# This is a simple function that generates a workplace with a specific number
-# of workers and receptionists. In the future we would expand the number of
-# worker types (e.g. cleaners and security guards), include visitors and
-# customers, and potentially have people leave the office to do things like
-# shopping through the day.
-def create_workplace(
-        n_workers,
-        n_receptionists=0,            #no receptionist
-        open_plan_area_per_worker = 7.,
-        open_plan_ceiling_height = 4.5,
-        open_plan_total_space = 180,
-        meeting_room_capacity = None,
-        add_visitors = False):
 
+# Set the Model up
+def create_workplace(n_singers):
     # create the workplace
     workplace = Workplace(BOX_TYPE, WORKER_TYPE)
-
-    # create the period generators
-#    lunch_generator        = LunchGenerator()
-#    toilet_break_generator = ToiletBreakGenerator()
-    working_generator      = WorkingGenerator()
-
-    # create the boxes
-#    if open_plan_total_space is None:
-#        open_plan_total_space = n_workers * open_plan_area_per_worker
-#    open_plan_total_space *= open_plan_ceiling_height
-
-#    if meeting_room_capacity is None:
-#        meeting_room_capacity = min(20, max(5, .5 * n_workers))
-
-    hall        = Box(180., BOX_TYPE.OPEN_PLAN , None, name='hall')
-
-    # add boxes to the workplace
+    hall = Box(180., BOX_TYPE.HALL , None, name='hall')
     workplace.add_box(hall)
 
-    #egress_generator = EgressGenerator(foyer)
+    workplace.add_generator(ChoralPractice(hall))
 
-    # workplace.add_generator(MeetingGenerator(
-    #     GATHERING_TYPE.MEETING,
-    #     [WORKER_TYPE.WORKER],
-    #     [BOX_TYPE.MEETING]))
-
-    if add_visitors:
-        # must be added after MeetingGenerator instance
-        workplace.add_generator(GatheringVisits(
-            Visitor,
-            [GATHERING_TYPE.MEETING],
-            [egress_generator],
-            visitor_count_generator))
-
-    # working_generator and egress_generator must be added last and in this order.
-    # TODO: might want to add two more setters on person for these
-#    generators = [lunch_generator, toilet_break_generator, working_generator, egress_generator]
-    generators = [working_generator]
-    def configure_person(person, **kwargs):
-#         person.kitchen = kitchen
-#        person.toilet  = toilet
-
-        person.add_generators(generators)
-
-        for k, v in kwargs: setattr(person, k, v)
-        workplace.add_person(person)
-
-#    for i in range(n_receptionists):
-#        configure_person(Person(48, role=WORKER_TYPE.RECEPTIONIST, work_box=foyer))
-    for i in range(n_workers):
-        configure_person(Person(48, role=WORKER_TYPE.CHORUS, work_box=hall))
+#    if add_visitors:
+#        # must be added after MeetingGenerator instance
+#        workplace.add_generator(GatheringVisits(
+#            Visitor,
+#            [GATHERING_TYPE.MEETING],
+#            [egress_generator],
+#            visitor_count_generator))
+#
+#    # working_generator and egress_generator must be added last and in this order.
+#    # TODO: might want to add two more setters on person for these
+##    generators = [lunch_generator, toilet_break_generator, working_generator, egress_generator]
+#    generators = [working_generator]
+#    def configure_person(person, **kwargs):
+##         person.kitchen = kitchen
+##        person.toilet  = toilet
+#
+#        person.add_generators(generators)
+#
+#        for k, v in kwargs: setattr(person, k, v)
+#        workplace.add_person(person)
+#
+##    for i in range(n_receptionists):
+##        configure_person(Person(48, role=WORKER_TYPE.RECEPTIONIST, work_box=foyer))
+#    for i in range(n_workers):
+#        configure_person(Person(48, role=WORKER_TYPE.CHORUS, work_box=hall))
+    for i in range(n_singers):
+        workplace.add_person(Singer(48, role=WORKER_TYPE.CHORUS))
 
     return workplace
 
@@ -147,15 +108,10 @@ def create_workplace(
 # number of people in the meeting.
 _activity_weights = {
     # for workers
-    (WORKER_TYPE.CHORUS, BOX_TYPE.OPEN_PLAN)   : 'singing_voiced'
-    }
-
-
+    (WORKER_TYPE.CHORUS, BOX_TYPE.HALL) : 'singing_voiced'}
 
 _mask_wearing = {
-    (WORKER_TYPE.CHORUS, BOX_TYPE.OPEN_PLAN)  : False}
-
-
+    (WORKER_TYPE.CHORUS, BOX_TYPE.HALL) : False}
 
 def create_emissions_calculator(pathogen):
     return EmissionsCalculator(pathogen, _activity_weights, _mask_wearing)
